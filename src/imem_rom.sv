930154dbe1e0868ee8fa30ce31d9108b7cfee4df
Ron Jokai <ron.jokai@arm.com>
2026-01-09 23:26:23 -0600

    [Code org] Changes needed to run tests from dedicated dir to avoid cluttering repo root.

--- src/imem_rom.sv
+++ src/imem_rom.sv
@@ -7,7 +7,7 @@ module imem_rom #(parameter int DEPTH = 4096) (
 );
   logic [31:0] mem [0:DEPTH-1];
   //initial for (int i=0;i<DEPTH;i++) mem[i] = 32'h00000013; // ADDI x0,x0,0
-  initial $readmemh("./sw/fir.hex", mem);
+  initial $readmemh("../sw/fir.hex", mem);
   assign inst = mem[pc[31:2]];
 endmodule
 
